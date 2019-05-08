32612d8a38de1281ea103263efc5f6357c03c454
Maxfield <mffriedm@ucsc.edu>
2019-05-08 16:12:07 -0700

    Cow goes moo
    What does the cow say?

---
+++ Well.js
@@ -0,0 +1,13 @@
+var game = new Phaser.Game(1500, 800, Phaser.AUTO, 'phaser');
+
+//The variable for the main menu state
+var MainMenu = function(game){};
+
+// Using the phaser prototype to run the main menu state
+MainMenu.prototype = {
+
+	create: function()
+	{
+		 console.log("http://www.clovermeadowsbeef.com/amazing-facts-about-cows/");
+	}
+}
